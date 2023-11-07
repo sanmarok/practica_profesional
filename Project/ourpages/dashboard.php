f3ab91d6b1ba819764ef1bb558b850c884e6abce
Santiago Martinez <san.martinezok@gmail.com>
2023-11-07 08:48:36 -0300

    Dashboard ready

--- Project/ourpages/dashboard.php
+++ Project/ourpages/dashboard.php
@@ -1,13 +1,162 @@
 <?php
 session_start();
 if (isset($_SESSION['id'])) {
-    echo "ID: " . $_SESSION['id'] . "<br>";
-    echo "Nombre: " . $_SESSION['first_name'] . "<br>";
-    echo "Apellido: " . $_SESSION['last_name'] . "<br>";
-    echo "Rol: " . $_SESSION['role'] . "<br>";
 } else {
-    header('Location: authentication.php');
-    exit();
+  header('Location: authentication.php');
+  exit();
 }
 ?>
 
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Inicio</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+</head>
+
+<body class="layout-navbar-fixed control-sidebar-slide-open dark-mode">
+  <!-- Site wrapper -->
+  <div class="wrapper">
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-light bg-dark">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link text-white" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="dashboard.php" class="nav-link text-white">Inicio</a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="dashboard.php" class="brand-link elevation-4">
+        <span class="brand-text font-weight-light mx-auto">Infinet</span>
+      </a>
+
+      <!-- Sidebar -->
+      <div class="sidebar">
+        <!-- Sidebar user (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div class="info">
+            <a href="#" class="d-block"><?php echo $_SESSION['first_name'] . ' ' . $_SESSION['last_name'] ?></a>
+            <?php
+            switch ($_SESSION['role']) {
+              case 1:
+                echo '<i class="nav-icon fas fa-user-tie"></i><span class= px-3>Adminstrador<span>';
+                break;
+              case 2:
+                echo '<i class="nav-icon fas fa-user-gear"></i><span class= px-3>Tecnico<span>';
+                break;
+              case 3:
+                echo '<i class="nav-icon fas fa-user"></i><span class= px-3>Secretario<span>';
+                break;
+              default:
+                # code...
+                break;
+            }
+            ?>
+          </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+            <li class="nav-item">
+              <a href="../clientes.html" class="nav-link">
+                <i class="nav-icon fas fa-user"></i>
+                <p>
+                  Clientes
+                </p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="../servicios.html" class="nav-link">
+                <i class="nav-icon fas fa-cogs"></i>
+                <p>
+                  Servicios
+                </p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="../solicitudes-tecnicas.html" class="nav-link">
+                <i class="nav-icon fas fa-tools"></i>
+                <p>
+                  Solicitudes Técnicas
+                </p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="../pedidos-compra.html" class="nav-link">
+                <i class="nav-icon fas fa-shopping-cart"></i>
+                <p>
+                  Pedidos de Compra
+                </p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="../productos.html" class="nav-link">
+                <i class="nav-icon fas fa-box-open"></i>
+                <p>
+                  Productos
+                </p>
+              </a>
+            </li>
+
+
+          </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+      </div>
+      <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- jQuery -->
+  <script src="../plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="../dist/js/demo.js"></script>
+</body>
+
+</html>
