fa4fc982d48675834b75e4c0b33acedc5d2902f3
Santiago Martinez <san.martinezok@gmail.com>
2023-12-04 22:32:20 -0300

    Fix styles

--- Project/ourwidgets/profile_client/form_edit_client.php
+++ Project/ourwidgets/profile_client/form_edit_client.php
@@ -44,126 +44,115 @@ $mysqli->close();
         </div>
     </div>
     <!-- /.card-header -->
+
     <div class="card-body">
-        <div class="card-body">
-            <form method="post" class="needs-validation" novalidate autocomplete="off">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <!-- Nombres -->
-                        <div class="form-group input-group">
-                            <label for="inputFirstName">Nombres</label>
-                            <div class="input-group">
-                                <input type="text" class="form-control form-control-border " name="first_name"
-                                    id="inputFirstName" value="<?php echo $first_name; ?>" required disabled>
-                                <span class="input-group-append">
-                                    <button class="btn btn-outline-danger mx-2" type="button" id="editFirstName"><i
-                                            class="fas fa-pencil-alt"></i></button>
-                                </span>
-                                <div class="invalid-tooltip">
-                                    Por favor, Ingrese un Nombre.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <!-- Apellidos -->
-                        <div class="form-group input-group">
-                            <label for="inputLastName">Apellidos</label>
-                            <div class="input-group">
-                                <input type="text" class="form-control form-control-border" name="last_name"
-                                    id="inputLastName" value="<?php echo $last_name; ?>" required disabled>
-                                <span class="input-group-append">
-                                    <button class="btn btn-outline-danger mx-2" type="button" id="editLastName"><i
-                                            class="fas fa-pencil-alt"></i></button>
-                                </span>
-                                <div class="invalid-tooltip">
-                                    Por favor, Ingrese un Apellido.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-sm-6">
-                        <!-- Teléfono -->
-                        <div class="form-group input-group">
-                            <label for="inputPhone">Teléfono</label>
-                            <div class="input-group">
-                                <input type="text" class="form-control form-control-border" name="phone" id="inputPhone"
-                                    value="<?php echo $phone; ?>" required disabled>
-                                <span class="input-group-append">
-                                    <button class="btn btn-outline-danger mx-2" type="button" id="editPhone"><i
-                                            class="fas fa-pencil-alt"></i></button>
-                                </span>
-                                <div class="invalid-tooltip">
-                                    Por favor, Ingrese un numero de Telefono.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <!-- Correo Electrónico -->
-                        <div class="form-group input-group">
-                            <label for="inputEmail">Correo Electrónico</label>
-                            <div class="input-group">
-                                <input type="email" class="form-control form-control-border" name="email"
-                                    id="inputEmail" value="<?php echo $email; ?>" required disabled>
-                                <span class="input-group-append">
-                                    <button class="btn btn-outline-danger mx-2" type="button" id="editEmail"><i
-                                            class="fas fa-pencil-alt"></i></button>
-                                </span>
-                                <div class="invalid-tooltip">
-                                    Por favor, Ingrese un Correo Electrónico.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-6">
-                        <!-- Documento -->
-                        <div class="form-group input-group">
-                            <label for="inputDocumento">Documento</label>
-                            <div class="input-group">
-                                <input type="text" class="form-control form-control-border" name="document"
-                                    id="inputDocumento" value="<?php echo $document; ?>" required disabled>
-                                <span class="input-group-append">
-                                    <button class="btn btn-outline-danger mx-2" type="button" id="editDocumento"><i
-                                            class="fas fa-pencil-alt"></i></button>
-                                </span>
-                                <div class="invalid-tooltip">
-                                    Por favor, Ingrese un Numero de Documento.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <!-- Estado -->
-                        <div class="form-group input-group">
-                            <div class="input-group">
-                                <label for="inputState">Estado</label>
-                            </div>
-                            <select class="custom-select form-control-border form-select" name="state" id="inputState"
-                                disabled>
-                                <option value="1" <?php echo $state == 1 ? 'selected' : ''; ?>>Activo</option>
-                                <option value="0" <?php echo $state == 0 ? 'selected' : ''; ?>>Inactivo</option>
-                            </select>
+        <form method="post" class="needs-validation" novalidate autocomplete="off">
+            <div class="row">
+                <div class="col-sm-6">
+                    <!-- Nombres -->
+                    <div class="form-group input-group">
+                        <label for="inputFirstName">Nombres</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control form-control-border " name="first_name" id="inputFirstName" value="<?php echo $first_name; ?>" required disabled>
                             <span class="input-group-append">
-                                <button class="btn btn-outline-danger mx-2" type="button" id="editState"><i
-                                        class="fas fa-pencil-alt"></i></button>
+                                <button class="btn btn-outline-danger mx-2" type="button" id="editFirstName"><i class="fas fa-pencil-alt"></i></button>
                             </span>
+                            <div class="invalid-tooltip">
+                                Por favor, Ingrese un Nombre.
+                            </div>
                         </div>
-
                     </div>
-                    <input type="hidden" id="inputId" name="id" value="<?php echo $client_id; ?>">
                 </div>
-                <div class="form-footer">
-                    <button id="btnGuardar" type="submit" class="btn btn-success float-right" disabled>Guardar</button>
+                <div class="col-sm-6">
+                    <!-- Apellidos -->
+                    <div class="form-group input-group">
+                        <label for="inputLastName">Apellidos</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control form-control-border" name="last_name" id="inputLastName" value="<?php echo $last_name; ?>" required disabled>
+                            <span class="input-group-append">
+                                <button class="btn btn-outline-danger mx-2" type="button" id="editLastName"><i class="fas fa-pencil-alt"></i></button>
+                            </span>
+                            <div class="invalid-tooltip">
+                                Por favor, Ingrese un Apellido.
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </form>
-        </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-6">
+                    <!-- Teléfono -->
+                    <div class="form-group input-group">
+                        <label for="inputPhone">Teléfono</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control form-control-border" name="phone" id="inputPhone" value="<?php echo $phone; ?>" required disabled>
+                            <span class="input-group-append">
+                                <button class="btn btn-outline-danger mx-2" type="button" id="editPhone"><i class="fas fa-pencil-alt"></i></button>
+                            </span>
+                            <div class="invalid-tooltip">
+                                Por favor, Ingrese un numero de Telefono.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <!-- Correo Electrónico -->
+                    <div class="form-group input-group">
+                        <label for="inputEmail">Correo Electrónico</label>
+                        <div class="input-group">
+                            <input type="email" class="form-control form-control-border" name="email" id="inputEmail" value="<?php echo $email; ?>" required disabled>
+                            <span class="input-group-append">
+                                <button class="btn btn-outline-danger mx-2" type="button" id="editEmail"><i class="fas fa-pencil-alt"></i></button>
+                            </span>
+                            <div class="invalid-tooltip">
+                                Por favor, Ingrese un Correo Electrónico.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <!-- Documento -->
+                    <div class="form-group input-group">
+                        <label for="inputDocumento">Documento</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control form-control-border" name="document" id="inputDocumento" value="<?php echo $document; ?>" required disabled>
+                            <span class="input-group-append">
+                                <button class="btn btn-outline-danger mx-2" type="button" id="editDocumento"><i class="fas fa-pencil-alt"></i></button>
+                            </span>
+                            <div class="invalid-tooltip">
+                                Por favor, Ingrese un Numero de Documento.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <!-- Estado -->
+                    <div class="form-group input-group">
+                        <div class="input-group">
+                            <label for="inputState">Estado</label>
+                        </div>
+                        <select class="custom-select form-control-border form-select" name="state" id="inputState" disabled>
+                            <option value="1" <?php echo $state == 1 ? 'selected' : ''; ?>>Activo</option>
+                            <option value="0" <?php echo $state == 0 ? 'selected' : ''; ?>>Inactivo</option>
+                        </select>
+                        <span class="input-group-append">
+                            <button class="btn btn-outline-danger mx-2" type="button" id="editState"><i class="fas fa-pencil-alt"></i></button>
+                        </span>
+                    </div>
+
+                </div>
+                <input type="hidden" id="inputId" name="id" value="<?php echo $client_id; ?>">
+            </div>
     </div>
-    <?php include("../functions/update_client.php"); ?>
-    <!-- /.card-body -->
+    <div class="card-footer">
+        <button id="btnGuardar" type="submit" class="btn btn-success float-right" disabled>Guardar</button>
+    </div>
+    </form>
+</div>
+
+<?php include("../functions/update_client.php"); ?>
+<!-- /.card-body -->
 </div>
